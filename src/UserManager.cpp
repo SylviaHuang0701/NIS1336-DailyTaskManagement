6efc7666b379700e972857cdabfbee76c266e633
SylviaHuang0701 <3192043878@qq.com>
2024-07-05 00:29:26 +0800

    Initial commit

---
+++ src/UserManager.cpp
@@ -0,0 +1,25 @@
+#include "UserManager.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+bool UserManager::login(const std::string& username, const std::string& password) {
+
+}
+
+bool UserManager::registerUser(const std::string& username, const std::string& password) {
+
+}
+
+void UserManager::loadUsers() {
+
+}
+
+void UserManager::saveUsers() {
+
+}
+
+std::string UserManager::hashPassword(const std::string& password) {
+
+}
